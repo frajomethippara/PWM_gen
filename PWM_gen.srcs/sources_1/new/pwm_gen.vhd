1be354dae76e34f7eafc13c386ea3433d88ba872
unknown <francisjohn097@gmail.com>
2024-11-04 12:14:04 +0100

    Initial commit. Added vivado project files

---
+++ PWM_gen.srcs/sources_1/new/pwm_gen.vhd
@@ -0,0 +1,15 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity pwm_gen is
+    Port ( clk : in STD_LOGIC;
+           rst : in STD_LOGIC;
+           pwm_gen_out : out STD_LOGIC);
+end pwm_gen;
+
+architecture Behavioral of pwm_gen is
+
+begin
+
+
+end Behavioral;
