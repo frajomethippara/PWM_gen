856efe108369ed76d12d7973c869f0107c5dc814
unknown <francisjohn097@gmail.com>
2024-11-12 00:49:42 +0100

    Remove large .wdb file and update .gitignore

--- PWM_gen.srcs/sim_1/new/pwm_gen_testbench.v
+++ PWM_gen.srcs/sim_1/new/pwm_gen_testbench.v
@@ -1,23 +1,24 @@
 `timescale 1ns / 1ps
 
-module pwm_gen_testbench
+module pwm_gen_testbench();
+
     reg clk;
     reg rst;
     wire pwm_gen_out;
     
     pwm_gen dut(
-    .clk(clk),
-    .rst(rst),
-    .pwm_gen_out(pwm_gen_out)
+        .clk(clk),
+        .rst(rst),
+        .pwm_gen_out(pwm_gen_out)
     );
     
-    
-initial begin
-    clk = 0;
-    rst = 1;
-    #500;
-    rst = 0;
-end
+    initial begin
+        clk = 0;
+        rst = 1;
+        #500;
+        rst = 0;
+    end
 
-always #2.5 clk = ~clk;
+    always #2.5 clk = ~clk;
+    
 endmodule
